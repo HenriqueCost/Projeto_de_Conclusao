d1af36fded32a02329ac5cf1bf4db23ad99d0574
Henrique Costa <henrique.costa@ibm.com>
2022-07-07 12:00:55 -0300

    Criacao da classe PagebleResponse

---
+++ src/main/java/com/henriquecosta/criacao_de_api/wrapper/PageableResponse.java
@@ -0,0 +1,38 @@
+package com.henriquecosta.criacao_de_api.wrapper;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class PageableResponse<T> extends PageImpl<T> {
+    private boolean first;
+    private boolean last;
+    private int totalPages;
+    private int numberOfElements;
+
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public PageableResponse(@JsonProperty("content") List<T> content,
+                            @JsonProperty("number") int number,
+                            @JsonProperty("size") int size,
+                            @JsonProperty("totalElements") int totalElements,
+                            @JsonProperty("last") boolean last,
+                            @JsonProperty("first") boolean first,
+                            @JsonProperty("totalPages") int totalPages,
+                            @JsonProperty("numberOfElements") int numberOfElements){
+        super(content, PageRequest.of(number, size), totalElements);
+
+        this.last = last;
+        this.first = first;
+        this.totalPages = totalPages;
+        this.numberOfElements = numberOfElements;
+    }
+}
--- src/test/java/com/henriquecosta/criacao_de_api/integration/MunicipalityIntegration.java
+++ src/test/java/com/henriquecosta/criacao_de_api/integration/MunicipalityIntegration.java
@@ -1,4 +1,40 @@
 package com.henriquecosta.criacao_de_api.integration;
 
-public class MunicipalityIntegration {
+import com.henriquecosta.criacao_de_api.entities.Municipality;
+import com.henriquecosta.criacao_de_api.utils.MunicipalityTools;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.HttpMethod;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase
+class MunicipalityIntegration {
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    @DisplayName("Pagina em Integration retorna lista de dados quando sucesso")
+    void encontrarPorPagina_Integration_deve_retornar_listaDeDados_quando_ha_sucesso(){
+        //Caso o "pegarPorPagina" não tenha sucesso o mesmo exige um nome valido
+        String nomeProdutoEsperado = MunicipalityTools.CriarNovoDadoQueSejaValido().getNomeProduto();
+        template.exchange("/dados", HttpMethod.GET, null,
+                new ParameterizedTypeReference<PageImpl<Municipality>>() {});
+        //As Assertions fazem essa exigência
+        /*Assertions.assertThat(dadosLista).isNotNull();
+        Assertions.assertThat(dadosLista.toList()).isNotEmpty().hasSize(1);
+        Assertions.assertThat(dadosLista.toList().get(0).getNomeProduto()).isEqualTo(nomeProdutoEsperado);*/
+    }
 }
